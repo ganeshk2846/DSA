121ca2441c5d21eebe8d02e43c52ea0ca0ca5ac3
Ganesh <ganeshkarrolla2846@gmail.com>
2026-07-31 23:28:59 +0530

    Time: 4 ms (82.67%), Space: 44.4 MB (66.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length())
+            return false;
+            
         int s_freq[]=new int[26];
-        int t_freq[]=new int[26];
 
         for(char ch:s.toCharArray())
             s_freq[ch-'a']++;
         
         for(char ch:t.toCharArray())
-            t_freq[ch-'a']++;
-
-        for(int i=0;i<26;i++)
         {
-            if(s_freq[i] != t_freq[i])
-                return false;
-        }
+            if(s_freq[ch-'a'] <=0)
+            return false;
 
+            s_freq[ch-'a']--;
+
+        }
         return true;
     }
 }
